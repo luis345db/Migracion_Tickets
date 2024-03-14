15bbdd82930a61d68381ad4cbe46f3de3e760ca3
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-14 00:27:48 -0400

    Hize el conetion string y inyecte el contexto

--- TicketsApp/TicketsApp.Api/Program.cs
+++ TicketsApp/TicketsApp.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TicketsApp.Api.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var Constr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(option => option.UseSqlite(Constr));
+
 
 var app = builder.Build();
 
