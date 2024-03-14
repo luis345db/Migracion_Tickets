3bc74ad98ba1d5083b11b4b0fb6ea8d3d3318982
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-13 23:48:25 -0400

    Cree el modelo de Prioridades

---
+++ TicketsApp/Shared/Models/Prioridades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Shared.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadesId { get; set; }
+        [Required(ErrorMessage = "Es requerido")]
+        public string? Descripcion { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "El día debe ser mayor a cero")]
+        public int DiasCompromiso { get; set; }
+
+    }
+}
