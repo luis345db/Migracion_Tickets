d971d4fbdc420c94af4552e94798f2ab0605e943
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-14 00:11:09 -0400

    Cree el modelo de el detalle

---
+++ TicketsApp/Shared/Models/Tickets_Detalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Tickets_Detalle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("TicketId")]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Es requerido")]
+        public string? Emisor { get; set; }
+
+        [Required(ErrorMessage = "Es requerido")]
+        public string? Mensaje { get; set; }
+    }
+}
