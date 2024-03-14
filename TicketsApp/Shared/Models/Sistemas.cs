85366728c4bd33c4a0dbdc7c71fe8c41fe3b744d
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-14 00:05:54 -0400

    Cree el modelo de Sistemas

---
+++ TicketsApp/Shared/Models/Sistemas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemasId { get; set; }
+        [Required(ErrorMessage = "Es requerido")]
+        public string? Nombre { get; set; }
+    }
+}
